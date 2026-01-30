b7cfd6c5ce172568934805759d4381a1e5ad846c
uzumakimooham <132150393+uzumakimooham@users.noreply.github.com>
2026-01-30 16:25:36 +0700

    Add files via upload

---
+++ CSharp/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp;
+/*public class Person
+{
+    private string name = "";
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+}*/
+
+public class Person
+{/*
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }*/
+    public string Name { get; set; }
+
+}
+
+
+
+public class Person1
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+    public Person1(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+public class Person2
+{
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+}
+
+public class Person3
+{
+    public required string Name { get; init; }
+    public required int Age { get; init; }
+}
+
---
+++ CSharp/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp;
+
+public class Point
+{
+    public double X { get; }
+    public double Y { get; }
+    public override string ToString() => $"({X}, {Y})";
+
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static Point operator +(Point left, Point right)
+    {
+        return new (left.X + right.X, left.Y + right.Y);
+    }
+
+    public double GetDistance()
+    {
+        return Math.Sqrt(X * X + Y * Y);
+    }
+}
